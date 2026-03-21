ac09d9315d9c697aa6330d4cc82accf8ea8cdb3a
Shannon <liushannon56@gmail.com>
2026-03-20 22:53:33 -0400

    define product model in models.py

--- src/models.py
+++ src/models.py
@@ -2,22 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Define Episode model
-class Episode(db.Model):
-    __tablename__ = 'episodes'
+class Product(db.Model):
+    __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64), nullable=False)
-    descr = db.Column(db.String(1024), nullable=False)
-    
-    def __repr__(self):
-        return f'Episode {self.id}: {self.title}'
+    original_id = db.Column(db.String(32))
+    name = db.Column(db.String(256), nullable=False)
+    brand = db.Column(db.String(128), nullable=False)
+    category = db.Column(db.String(128), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Float)
+    num_reviews = db.Column(db.Integer)
+    details = db.Column(db.Text)
+    ingredients = db.Column(db.Text)
+    url = db.Column(db.String(512))
+    size = db.Column(db.String(128))
+    online_only = db.Column(db.Boolean, default=False)
 
-# Define Review model
-class Review(db.Model):
-    __tablename__ = 'reviews'
-    id = db.Column(db.Integer, primary_key=True)
-    imdb_rating = db.Column(db.Float, nullable=False)
-    
-    def __repr__(self):
-        return f'Review {self.id}: {self.imdb_rating}'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'brand': self.brand,
+            'category': self.category,
+            'price': self.price,
+            'rating': self.rating,
+            'num_reviews': self.num_reviews,
+            'details': self.details,
+            'ingredients': self.ingredients,
+            'url': self.url,
+            'size': self.size,
+            'online_only': self.online_only,
+        }
 
+    def __repr__(self):
+        return f'<Product {self.id}: {self.name}>'
